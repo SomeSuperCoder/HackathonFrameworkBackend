e2ffbd66eec28f1731f03db363ee41108345aac1
SomeSuperCoder <ithelper9@gmail.com>
2025-10-02 11:06:41 +0300

    Grades access check

--- internal/validators/team_validator.go
+++ internal/validators/team_validator.go
@@ -21,13 +21,36 @@ func NewTeamValidator(userAuth *models.User, team *models.Team) *TeamValidator {
 	// Access
 	tv.av.validator.RegisterValidation("owner", tv.validateOwner)
 	// Other
-	tv.av.validator.RegisterValidation("grades", models.ValidateGrades)
+	tv.av.validator.RegisterValidation("grades", tv.validateGrades)
 
 	return tv
 }
 
 func (tv *TeamValidator) validateOwner(fl validator.FieldLevel) bool {
 	return tv.av.isAdmin() || tv.team.Leader == tv.userAuth.ID
+}
+
+func (tv *TeamValidator) validateGrades(fl validator.FieldLevel) bool {
+	// Get the field value and check if it's the correct type
+	if nestedMap, ok := fl.Field().Interface().(models.Grades); ok {
+		// Iterate through the outer map
+		for k, innerMap := range nestedMap {
+			// Check judge access
+			if k != tv.userAuth.ID {
+				return false
+			}
+
+			// Iterate through each inner map
+			for _, value := range innerMap {
+				// Check if the integer value is within the required range
+				if value > 10 {
+					return false
+				}
+			}
+		}
+		return true // All values are valid
+	}
+	return false // Type assertion failed
 }
 
 func (tv *TeamValidator) ValidateRequest(r any) error {
--- models/team.go
+++ models/team.go
@@ -3,29 +3,10 @@ package models
 import (
 	"time"
 
-	"github.com/go-playground/validator/v10"
 	"go.mongodb.org/mongo-driver/v2/bson"
 )
 
 type Grades map[bson.ObjectID]map[bson.ObjectID]uint16
-
-func ValidateGrades(fl validator.FieldLevel) bool {
-	// Get the field value and check if it's the correct type
-	if nestedMap, ok := fl.Field().Interface().(Grades); ok {
-		// Iterate through the outer map
-		for _, innerMap := range nestedMap {
-			// Iterate through each inner map
-			for _, value := range innerMap {
-				// Check if the integer value is within the required range
-				if value > 10 {
-					return false
-				}
-			}
-		}
-		return true // All values are valid
-	}
-	return false // Type assertion failed
-}
 
 type Team struct {
 	ID              bson.ObjectID `bson:"_id,omitempty" json:"_id"`
